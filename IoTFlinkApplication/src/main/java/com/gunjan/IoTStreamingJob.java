b3b45c036d90a123fc5defce94e7fd6072bc446b
Gunjan Vishwakarma <gunjan.it2k4@gmail.com>
2021-04-27 16:52:21 +0530

    Initial Commit 3

--- IoTFlinkApplication/src/main/java/com/gunjan/IoTStreamingJob.java
+++ IoTFlinkApplication/src/main/java/com/gunjan/IoTStreamingJob.java
@@ -1,15 +1,22 @@
 package com.gunjan;
 
-import com.influxdb.client.domain.WritePrecision;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.influxdb.client.write.Point;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.ContinuousProcessingTimeTrigger;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
+import org.apache.flink.util.Collector;
+import scala.Tuple2;
 
-import java.time.Instant;
 import java.util.Properties;
 
 /**
@@ -30,11 +37,36 @@ public class IoTStreamingJob {
     public static void main(String[] args) throws Exception {
 
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStream<String> input = createSourceFromStaticConfig(env);
-        input.map((MapFunction<String, Point>) s -> Point.measurement("devicedata")
-                .addTag("payload", s)
-                .addField("value", 55D)
-                .time(Instant.now().toEpochMilli(), WritePrecision.MS)).addSink(new InfluxDBSink());
+        createSourceFromStaticConfig(env)
+                .map((MapFunction<String, FlinkDevicePayload>) payload -> new ObjectMapper().readValue(payload, FlinkDevicePayload.class))
+                .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<FlinkDevicePayload>(Time.seconds(300)) {
+                    @Override
+                    public long extractTimestamp(FlinkDevicePayload element) {
+                        return element.getReceived_at();
+                    }
+                })
+                .timeWindowAll(Time.seconds(30), Time.seconds(5))
+                .trigger(ContinuousProcessingTimeTrigger.of(Time.seconds(5)))
+                .apply(new AllWindowFunction<FlinkDevicePayload, Tuple2<Long, Long>, TimeWindow>() {
+                    Double totalTemp = 0.0;
+                    Long count = 0L;
+                    @Override
+                    public void apply(TimeWindow timeWindow, Iterable<FlinkDevicePayload> iterable, Collector<Tuple2<Long, Long>> collector) throws Exception {
+                        iterable.forEach(flinkDevicePayload -> {
+                            final Double temperature = (Double) flinkDevicePayload.getPayloadJson().get("temperature");
+                            totalTemp += temperature;
+                            count++;
+                        });
+                        final Long l = totalTemp.longValue() / count.longValue();
+
+                        collector.collect(new Tuple2<>(l, timeWindow.maxTimestamp()));
+                    }
+                }).map((MapFunction<Tuple2<Long, Long>, Point>) longLongTuple2 -> Point.measurement("avg_temperature")
+                        .addField("value", String.valueOf(longLongTuple2._1))
+                        .addField("time", longLongTuple2._2)).addSink(new InfluxDBSink());
+
         env.execute("IoTDataProcessing");
     }
 }
+
+
